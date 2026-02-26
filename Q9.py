4fc0920807ddd6a495647c48f3cf47c701cc07b6
manyaagrawal-2701 <manyaagrawal2701@gmail.com>
2026-02-27 00:41:42 +0530

    Add files via upload

---
+++ Q9.py
@@ -0,0 +1,10 @@
+scores=[30,40,70,60,90,99,50,96] # list of scores
+i=0
+while i<len(scores): # loop till end of list
+    if(scores[i]>=60): # condition to pass if score>=60
+        print("Pass")
+    else:
+        print("Fail")
+    i=i+1 # increasing value of i by 1
+      
+      
